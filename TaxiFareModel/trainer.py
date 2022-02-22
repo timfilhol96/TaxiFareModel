1f5dc36a23795d9cf05c36445383c41910f79ca4
Timothee Filhol <timfilhol9@gmail.com>
2022-02-22 15:40:57 +0100

    improved model

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -2,15 +2,27 @@ from sklearn import pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression,Ridge,SGDRegressor
+from sklearn.ensemble import AdaBoostRegressor,GradientBoostingRegressor,RandomForestRegressor,BaggingRegressor
+from sklearn.svm import LinearSVR, NuSVR,SVR
+
+from xgboost import XGBRegressor
+
+from sklearn.model_selection import train_test_split,cross_val_score
 from TaxiFareModel.data import clean_data, get_X_y,get_data
+
+import joblib
+
+from memoized_property import memoized_property
+
+import mlflow
+from mlflow.tracking import MlflowClient
 
 from TaxiFareModel.encoders import DistanceTransformer,TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
 
 class Trainer():
-    def __init__(self, X, y):
+    def __init__(self, X, y,experiment_name):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -18,8 +30,9 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name=experiment_name
 
-    def set_pipeline(self):
+    def set_pipeline(self,model):
         """defines the pipeline as a class attribute"""
 
         dist_pipe = Pipeline([
@@ -36,13 +49,16 @@ class Trainer():
         ], remainder="drop")
         pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            ('linear_model', model)
         ])
         return pipeline
 
-    def run(self):
+    def cross_val_rmse(self,model):
+        return cross_val_score(self.set_pipeline(model),self.X,self.y,n_jobs=-1,scoring='neg_root_mean_squared_error').mean()
+
+    def run(self,model):
         """set and train the pipeline"""
-        self.pipeline=self.set_pipeline()
+        self.pipeline=self.set_pipeline(model)
         self.pipeline.fit(self.X, self.y)
         return self
 
@@ -53,10 +69,53 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         return rmse
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    def save_model(self,name):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self, f'{name}.joblib')
+        pass
+
+
 if __name__ == "__main__":
-    df=get_data()
-    cleaned_df=clean_data(df)
-    X,y=get_X_y(cleaned_df)
-    X_train,X_val,y_train,y_val=train_test_split(X,y,test_size=0.2)
-    rmse=Trainer(X_train,y_train).run().evaluate(X_val,y_val)
-    print(rmse)
+    model_dict={'linear':LinearRegression(),'ridge':Ridge(),'SGD':SGDRegressor(),'Ada':AdaBoostRegressor(),'GBR':GradientBoostingRegressor(),
+                'RandomForestRegressor':RandomForestRegressor(),'BaggingRegressor':BaggingRegressor(),
+                'LinearSVR':LinearSVR(), 'NUSVR':NuSVR(),'SVR':SVR(),'XGB':XGBRegressor(max_depth=3, n_estimators=50, learning_rate=0.1)}
+
+    for model in model_dict:
+        df=get_data()
+        cleaned_df=clean_data(df)
+        X,y=get_X_y(cleaned_df)
+        # X_train,X_val,y_train,y_val=train_test_split(X,y,test_size=0.2,random_state=42)
+        trainer=Trainer(X,y,"[FRANCE] [PARIS] [timfilhol] taxi_fare v2")
+
+        rmse=trainer.cross_val_rmse(model_dict[model])
+
+        trainer.save_model(model)
+
+        trainer.mlflow_log_metric("rmse", rmse)
+        trainer.mlflow_log_param("model", model)
+        trainer.mlflow_log_param("student_name", 'Timothée Filhol')
+
+        experiment_id = trainer.mlflow_experiment_id
+        print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
